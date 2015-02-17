ae64f7cfe8887765a1d8869450266a11334f285c
ben <ben@ros.io>
2015-02-17 13:08:48 -0700

    Add capture method for Mac OSX
    Add caputer method which runs command screencapture.  Check if it
    is available, if not fallback to gtk.

--- capture.py
+++ capture.py
@@ -9,20 +9,54 @@ import gtk.gdk
 import time
 import sys
 import config
+import os
 
 
 print "Starting Capture"
 print "================"
 
-w = gtk.gdk.get_default_root_window()
-sz = w.get_size()
+captureMethod = None
+w = None
+sz = None
+
+def useScreenCapture():
+    try:
+        return os.system("typeof screencapture")
+        return True
+    except StandError:
+        return False
+
+def useGtk():
+    try:
+        import gtk
+        w = gtk.gdk.get_default_root_window()
+        sz = w.get_size()
+
+        return True
+    except StandardError:
+        return False
+
+def takeScreenshot():
+    if captureMethod == None:
+        if useScreenCapture():
+            captureMethod = "screen"
+        elif useGtk():
+            captureMethod = "gtk"
+        else:
+            print "No capture methods available"
+            sys.exit(1)
+    
+    if captureMethod == "screen":
+        os.system("screencapture capture/screenshot_" + str(int(time.time())) + ".png")
+    elif captureMethod == "gtk":
+        pb = gtk.gdk.Pixbuf(gtk.gdk.COLORSPACE_RGB,False,8,sz[0],sz[1])
+        pb = pb.get_from_drawable(w,w.get_colormap(),0,0,0,0,sz[0],sz[1])
+        if (pb != None):
+            pb.save("capture/screenshot_"+ str(int(time.time())) +".png","png")
+            print "Screenshot " + str(i) + " saved."
+        else:
+            print "Unable to get the screenshot."
 
 for i in xrange(0, config.CAPTURE_NUM):
-  pb = gtk.gdk.Pixbuf(gtk.gdk.COLORSPACE_RGB,False,8,sz[0],sz[1])
-  pb = pb.get_from_drawable(w,w.get_colormap(),0,0,0,0,sz[0],sz[1])
-  if (pb != None):
-    pb.save("capture/screenshot_"+ str(int(time.time())) +".png","png")
-    print "Screenshot " + str(i) + " saved."
-  else:
-    print "Unable to get the screenshot."
-  time.sleep(config.CAPTURE_DELAY)
+    takeScreenshot();
+    time.sleep(config.CAPTURE_DELAY)
